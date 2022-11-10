13a76cf1f6cf5a228d41cca37ff668fe8b99f3ae
Rounak Rai <96898616+Rounak33@users.noreply.github.com>
2022-11-10 14:30:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ function validate(isSubmitted = false) {
         globalIsSubmitted = true
 
     }
-     
-    if(globalIsSubmitted){
+
+    if (globalIsSubmitted) {
         var letters = /^[a-zA-Z]*$/;
 
         if (firstName.length >= 3 && isNaN(firstName) && firstName.match(letters)) {
             document.getElementById('first-name-valid').style.display = 'block'
             document.getElementById('first-name-invalid').style.display = 'none'
-    
+
         }
         else {
             document.getElementById('first-name-invalid').style.display = 'block'
@@ -33,27 +33,27 @@ function validate(isSubmitted = false) {
         if (lastName.length >= 3 && isNaN(lastName) && lastName.match(letters)) {
             document.getElementById('last-name-valid').style.display = 'block'
             document.getElementById('last-name-invalid').style.display = 'none'
-    
+
         }
         else {
             document.getElementById('last-name-invalid').style.display = 'block'
             document.getElementById('last-name-valid').style.display = 'none'
             error = true
         }
-    
-        if (email.includes('@') && email.includes('.') && email.indexOf('@') != 0 && email.length - email.lastIndexOf('.') > 3) {
+
+        if (email.includes('@') && email.includes('.') && email.indexOf('@') != 0 && email.lastIndexOf('.') < email.length - 2 && email.lastIndexOf('.') >= email.length - 4) {
             document.getElementById('email-valid').style.display = 'block'
             document.getElementById('email-invalid').style.display = 'none'
         } else {
             document.getElementById('email-invalid').style.display = 'block'
             document.getElementById('email-valid').style.display = 'none'
             error = true
-    
+
         }
-    
+
         var get_num = String(phoneNumber).charAt(0);
         var first_num = Number(get_num)
-    
+
         if (phoneNumber.length === 10 && !isNaN(phoneNumber) && first_num > 5) {
             document.getElementById('phone-number-valid').style.display = 'block'
             document.getElementById('phone-number-invalid').style.display = 'none'
@@ -62,7 +62,7 @@ function validate(isSubmitted = false) {
             document.getElementById('phone-number-valid').style.display = 'none'
             error = true
         }
-    
+
         if (male_g || female || others) {
             document.getElementById('valid-gender').style.display = "block"
             document.getElementById('invalid-gender').style.display = "none"
@@ -71,8 +71,8 @@ function validate(isSubmitted = false) {
             document.getElementById('valid-gender').style.display = "none"
             error = true
         }
-    
-    
+
+
         if (country != 'None') {
             document.getElementById('country-valid').style.display = 'block'
             document.getElementById('country-invalid').style.display = 'none'
@@ -81,13 +81,13 @@ function validate(isSubmitted = false) {
             document.getElementById('country-valid').style.display = 'none'
             error = true
         }
-    
-    
+
+
         if (!error && isSubmitted) {
             alert('Your details have been saved successfully!')
-    
+
             document.getElementById('registration-form').reset()
-    
+
             let validFeedbacks = document.getElementsByClassName('valid-feedback')
             for (i = 0; i < validFeedbacks.length; i++) {
                 validFeedbacks[i].style.display = 'none'
@@ -99,6 +99,6 @@ function validate(isSubmitted = false) {
         }
     }
 
-   
+
 }
 
